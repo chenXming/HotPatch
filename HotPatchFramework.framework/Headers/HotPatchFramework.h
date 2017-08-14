1b474518b61666b9fd6e7e520a35351a4081a5c3
chenxiaoming <13716873821@163.com>
2017-08-14 14:53:02 +0800

    初始化framework

---
+++ HotPatchFramework.framework/Headers/HotPatchFramework.h
@@ -0,0 +1,19 @@
+//
+//  HotPatchFramework.h
+//  HotPatchFramework
+//
+//  Created by 陈小明 on 2017/8/14.
+//  Copyright © 2017年 陈小明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HotPatchFramework.
+FOUNDATION_EXPORT double HotPatchFrameworkVersionNumber;
+
+//! Project version string for HotPatchFramework.
+FOUNDATION_EXPORT const unsigned char HotPatchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HotPatchFramework/PublicHeader.h>
+
+#import <HotPatchFramework/HotPatchViewController.h>
---
+++ HotPatchFramework.framework/Headers/HotPatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  HotPatchViewController.h
+//  HotPatchFramework
+//
+//  Created by 陈小明 on 2017/8/14.
+//  Copyright © 2017年 陈小明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotPatchViewController : UIViewController
+
+@end
